77238f3c1a84d94915c3d79818e4fce1e6e373fa
aditya_baniya <itsadityabaniya@gmail.com>
2025-02-27 15:31:25 -0600

    ai frontend and backend folder created

---
+++ ai/spam_email_detector.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+# Read the CSV file (adjust encoding if needed)
+df = pd.read_csv('spam.csv', encoding='latin-1')
+
+# Show 5 random samples
+print(df.sample(5))
+
+# (Optional) Print the first few rows to get a sense of the data
+print(df.head())
