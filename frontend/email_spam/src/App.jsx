46dc0c4e24c310f5a86c2c2229b6adac03d77d1f
aditya_baniya <itsadityabaniya@gmail.com>
2025-02-28 08:43:30 -0600

    Deleted backend folder because i have my backend in ai folder

--- frontend/email_spam/src/App.jsx
+++ frontend/email_spam/src/App.jsx
@@ -1,12 +1,31 @@
-import { useState } from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [prediction, setPrediction] = useState("");
+
+  const handleSubmit = async () => {
+    const response = await axios.post("http://127.0.0.1:5000/predict", {
+      sms: inputText
+    });
+
+    setPrediction(response.data.prediction);
+  };
 
   return (
-    <>
-      
-    </>
-  )
+    <div className="App">
+      <h1>SMS Spam Classifier</h1>
+      <textarea
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+        placeholder="Enter your message here"
+      />
+      <button onClick={handleSubmit}>Predict</button>
+
+      {prediction && <h2>{prediction}</h2>}
+    </div>
+  );
 }
 
-export default App
+export default App;
